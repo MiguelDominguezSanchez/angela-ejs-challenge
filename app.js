d3bd58f2406890f2a7491db0896a2ca655f55462
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-03-12 16:39:20 +0100

    changelle 7 Solution

--- app.js
+++ app.js
@@ -48,6 +48,11 @@ app.get('/contact', function (req, res) {
 	res.render('contact', { contactContent: contactContent })
 })
 
+// Compose route
+app.get('/compose', function (req, res) {
+	res.render('compose')
+})
+
 app.listen(3000, function () {
 	console.log('Server started on port 3000')
 })
