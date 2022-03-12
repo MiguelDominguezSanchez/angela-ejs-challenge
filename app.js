447b9433fa319ac727296668ecd6ccf87c441d0e
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-03-12 17:17:34 +0100

    Challenge 10 Solution

--- app.js
+++ app.js
@@ -54,7 +54,10 @@ app.get('/compose', function (req, res) {
 })
 
 app.post('/compose', function (req, res) {
-	console.log(req.body.postTitle)
+	const post = {
+		title: req.body.postTitle,
+		content: req.body.postBody,
+	}
 })
 
 app.listen(3000, function () {
